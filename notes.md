3bd9480291cabd2df59b4e314750888529886ab4
smallfryy <holly.m.peck@gmail.com>
2016-06-13 15:44:50 -0700

    created controller, module in core.js and defined functions to handle todos. rendered FE view

---
+++ notes.md
@@ -0,0 +1,9 @@
+Based on the routes defines in server.js:
+
+HTTP Verb |   URL               | Description
+---------------------------------------------------
+GET       | /api/todos          | Get all todos
+---------------------------------------------------
+POST      | /api/todos          | Create one todo
+---------------------------------------------------
+DELETE    | /api/todos/:todo_id |  delete one todo
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // set up ========================
-var express  = require('express');
-var app      = express();                   // create our app w/ express
-var mongoose = require('mongoose');         // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+
+var express     = require('express');
+var app         = express();                    // create our app w/ express
+var mongoose    = require('mongoose');          // mongoose for mongodb
+var morgan      = require('morgan');            // log requests to the console (express4)
+var bodyParser  = require('body-parser');        // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
 // configuration =================
@@ -19,10 +20,75 @@ app.use(methodOverride());
 
 
 // define model ===================
+
 var Todo = mongoose.model('Todo', {
-  text: String
+  text : String
 });
 
-// listen (start app with node server.js) ======================================
+
+// RESTful routes ====================
+
+// api ===============================
+// get all todos
+
+  app.get('/api/todos', function(req, res) {
+  // use mongoose to get all todos in the database
+    Todo.find(function(err, todos) {
+  // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+      if (err)
+       res.send(err)
+       res.json(todos); // return all todos in JSON format // return all todos in JSON format
+    });
+  });
+
+    // create todo and send back all todos after creation
+  app.post('/api/todos', function(req, res) {
+
+  // create a todo, information comes from AJAX request from Angular
+    Todo.create({
+      text : req.body.text,
+      done : false
+    }, function(err, todo) {
+        if (err)
+          res.send(err);
+
+  // get and return all the todos after you create another
+      Todo.find(function(err, todos) {
+          if (err)
+            res.send(err)
+            res.json(todos);
+        });
+    });
+  });
+
+  // delete a todo
+  app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+      _id : req.params.todo_id
+        }, function(err, todo) {
+          if (err)
+            res.send(err);
+
+  // get and return all the todos after you create another
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+        res.json(todos);
+      });
+    });
+  });
+
+
+// application front end route
+  app.get('', function(req, res){
+    res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end
+  });
+
+
+
+
+// listen (start app with nodemon server.js) ==========
 app.listen(8080);
-console.log("App listening on port 8080. You are awesome");
+console.log("App listening on port 8080. You are awesome!");
+
+
